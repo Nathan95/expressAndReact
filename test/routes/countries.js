24d00190f40a809070ed8a4efd98ed6b69b6dafe
Nathan95 <nathan@mediazoo.tv>
2019-05-16 18:03:20 +0100

    completed SSR

---
+++ test/routes/countries.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var data = require('./data.js')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(data);
+});
+
+module.exports = router;
